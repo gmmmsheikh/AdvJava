fa7cb5548a285f7478490dce54226904de9faa99
gmmmsheikh <gmmmsheikh@gmail.com>
2021-01-25 19:56:51 -0800

    added hours and output format

--- timeProject/Clock.java
+++ timeProject/Clock.java
@@ -3,8 +3,15 @@ package timeProject;
 public class Clock {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		int minutes, seconds, hours; 
+		for(hours =0; hours < 24; hours ++ ) {
+			for (minutes = 0; minutes < 60; minutes++) {
+				for (seconds = 0; seconds < 60; seconds++)  {
+					System.out.print(hours + " hours " +minutes + " minutes " + seconds + " seconds");
+				}
+ 
+			} 
+		}
 	}
 
 }
